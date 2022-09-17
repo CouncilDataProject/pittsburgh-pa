8be355a61196c7d17d7c39e1b4082a47b3a79253
CouncilDataProjectServiceAccount <councildataproject@gmail.com>
2022-09-17 15:25:51 +0000

    Initial commit

---
+++ web/src/index.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { App, AppConfigProvider } from "@councildataproject/cdp-frontend";
+
+import "@councildataproject/cdp-frontend/dist/index.css";
+
+const config = {
+    firebaseConfig: {
+        options: {
+            projectId: "cdp-pittsburgh-pa-940bf40c",
+        },
+        settings: {},
+    },
+    municipality: {
+        name: "Pittsburgh",
+        timeZone: "America/New_York",
+        footerLinksSections: [],
+    },
+}
+
+ReactDOM.render(
+    <div>
+        <AppConfigProvider appConfig={config}>
+            <App />
+        </AppConfigProvider>
+    </div>,
+    document.getElementById("root")
+);
